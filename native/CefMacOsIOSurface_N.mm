9b071d214c54c847758ed6b01c5f1486eba1a3c1
木葉 Scarlet <93977077+mukjepscarlet@users.noreply.github.com>
2026-06-10 11:22:11 +0800

    Add CefMacOsIOSurface (OC++/Java)

---
+++ native/CefMacOsIOSurface_N.mm
@@ -0,0 +1,36 @@
+// Copyright (c) 2026 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#include <jni.h>
+
+#include <OpenGL/CGLIOSurface.h>
+#include <OpenGL/gl3.h>
+#include <OpenGL/OpenGL.h>
+
+JNIEXPORT jint JNICALL
+Java_org_cef_handler_CefMacOsIOSurface_bindToCurrentTexture(JNIEnv*,
+                                                            jclass,
+                                                            jlong io_surface,
+                                                            jint width,
+                                                            jint height) {
+  if (io_surface == 0 || width <= 0 || height <= 0) {
+    return kCGLBadValue;
+  }
+
+  CGLContextObj context = CGLGetCurrentContext();
+  if (context == nullptr) {
+    return kCGLBadContext;
+  }
+
+  return CGLTexImageIOSurface2D(
+      context,
+      GL_TEXTURE_RECTANGLE,
+      GL_RGBA,
+      width,
+      height,
+      GL_BGRA,
+      GL_UNSIGNED_INT_8_8_8_8_REV,
+      reinterpret_cast<IOSurfaceRef>(io_surface),
+      0);
+}
